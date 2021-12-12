a84e7bda1a7d11bc3add4ca65ce85d2abc1f4224
Justin Vanderschaaf <Vanderschaaf.usmc@gmail.com>
2021-12-12 17:03:56 -0600

    on delete loop and remove from array

--- client.js
+++ client.js
@@ -7,14 +7,37 @@ function onReady() {
   $("#employeeForm").on("submit", onAddEmployee);
 
   // Handle delete employee button
-  //$('.deleteBtn').on('click', onDeleteProduct);
   $(document).on("click", ".deleteBtn", onDeleteEmployee);
-}
+}//end onReady
 
 function onDeleteEmployee() {
-  console.log("👋 onDeleteEmployee");
-
+    let employeeId = $(this).closest("tr").attr("id");
+    console.log(employeeId);
+    console.log(employees);
+    //WTF??
+   for(let i = 0; i < employees.length; i++){
+       if(employees[i].id == employeeId){
+        employees.splice(i, 1);
+       }
+   }
+   
+   
+    console.log(employees);
   $(this).parents("tr").remove();
+  calculateTotal();
+}// end delete employee
+
+function calculateTotal(){
+    let totalWages = 0;
+  employees.forEach((employee) => (totalWages += employee.annualSalary));
+
+  let totalMonthly = Math.round((totalWages / 12 + Number.EPSILON) * 100) / 100;
+  $("#totalMonthly").text(`${totalMonthly}`);
+  if (totalMonthly > 20000) {
+    $("footer").css({
+      backgroundColor: "red",
+    });
+  }
 }
 
 function onAddEmployee(event) {
@@ -57,10 +80,10 @@ function onAddEmployee(event) {
   //loop through the employees array and render each as a <tr>
   for (let employee of employees) {
     $("#employeeList").append(`
-        <tr>
+        <tr id=${employee.id}>
              <td>${employee.firstName}</td>
               <td>${employee.lastName}</td>
-             <td>${employee.id}</td>
+             <td class="employeeId">${employee.id}</td>
              <td>${employee.title}</td>
              <td>${employee.annualSalary}</td>
              <td>
@@ -69,19 +92,12 @@ function onAddEmployee(event) {
                   </button>
             </td>
         </tr>`);
-  }
+  }//end employee loop
+  
   //add up total wages of employees
   //divide by 12
   //display to dom
-  let totalWages = 0;
-  employees.forEach((employee) => (totalWages += employee.annualSalary));
-
-  let totalMonthly = Math.round((totalWages / 12 + Number.EPSILON) * 100) / 100;
-  $("#totalMonthly").text(`${totalMonthly}`);
-  if (totalMonthly > 20000) {
-    $("footer").css({
-      backgroundColor: "red",
-    });
-  }
-}
+  calculateTotal();
+  
+}//end addEmployee
 $("#nameInput").val("");
